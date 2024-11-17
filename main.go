c431544a92feb7808f5c545669b9845459b017ca
Ashwin <ashwinbelbase@gmail.com>
2024-11-17 22:31:57 +0545

    Change Direction. Implement project to be like GlassWire
    Changed project direction to mimic Glasswire instead of TCPDUMP

--- main.go
+++ main.go
@@ -17,10 +17,6 @@ var rootCmd = &cobra.Command{
 	Short: "TCPDump in Go",
 	Long:  `A simple TCPDUMP implementation in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			cmd.Help()
-			return
-		}
 	},
 }
 
@@ -34,6 +30,27 @@ func init() {
 	cobra.OnInitialize(utils.ParseConfigs())
 	rootCmd.PersistentFlags().StringP("interface", "i", "eth0", "Interface to listen on")
 	viper.BindPFlag("interface", rootCmd.PersistentFlags().Lookup("interface"))
+}
+
+func decodeHTTP(payload string) {
+	if strings.HasPrefix(payload, "GET ") || strings.HasPrefix(payload, "POST ") ||
+		strings.HasPrefix(payload, "HTTP/1.1") || strings.HasPrefix(payload, "HTTP/1.0") {
+
+		log.Println("HTTP Packet Detected")
+
+		// Extract headers and body from payload
+		headersEnd := strings.Index(payload, "\r\n\r\n")
+		if headersEnd != -1 {
+			headers := payload[:headersEnd]
+			body := payload[headersEnd+4:]
+
+			log.Printf("HTTP Headers:\n%s\n", headers)
+			log.Printf("HTTP Body:\n%s\n", body)
+		} else {
+			// No body found, print entire payload as headers
+			log.Printf("HTTP Headers (No Body Detected):\n%s\n", payload)
+		}
+	}
 }
 
 func main() {
@@ -50,94 +67,34 @@ func main() {
 			if packet == nil {
 				continue
 			}
-			tcpLayer := packet.Layer(layers.LayerTypeTCP)
-			if tcpLayer == nil {
-				continue
-			}
-			tcp, _ := tcpLayer.(*layers.TCP)
-			payload := string(tcp.Payload)
-			if strings.HasPrefix(payload, "GET ") || strings.HasPrefix(payload, "POST ") ||
-				strings.HasPrefix(payload, "HTTP/1.1") || strings.HasPrefix(payload, "HTTP/1.0") {
 
-				log.Println("HTTP Packet Detected")
-
-				// Extract headers and body from payload
-				headersEnd := strings.Index(payload, "\r\n\r\n")
-				if headersEnd != -1 {
-					headers := payload[:headersEnd]
-					body := payload[headersEnd+4:]
-
-					log.Printf("HTTP Headers:\n%s\n", headers)
-					log.Printf("HTTP Body:\n%s\n", body)
-				} else {
-					// No body found, print entire payload as headers
-					log.Printf("HTTP Headers (No Body Detected):\n%s\n", payload)
+			if ipLayer := packet.NetworkLayer(); ipLayer != nil {
+				ip, ok := ipLayer.(*layers.IPv4)
+				if !ok {
+					log.Println("Not an IPv4 packet")
+					continue
 				}
+				proto, srcPort, dstPort := "", 0, 0
+				if tcpLayer := packet.TransportLayer(); tcpLayer != nil {
+					switch tcpLayer.LayerType() {
+					case layers.LayerTypeTCP:
+						proto = "TCP"
+						tcp, _ := tcpLayer.(*layers.TCP)
+						srcPort, dstPort = int(tcp.SrcPort), int(tcp.DstPort)
+					case layers.LayerTypeUDP:
+						proto = "UDP"
+						udp, _ := tcpLayer.(*layers.UDP)
+						srcPort, dstPort = int(udp.SrcPort), int(udp.DstPort)
+					default:
+						proto = "UNKNOWN"
+						srcPort, dstPort = 0, 0
+					}
+
+					log.Printf("%s:%d --%s--> %s:%d", ip.SrcIP.String(), srcPort, proto, ip.DstIP.String(), dstPort)
+				}
+
 			}
+
 		}
 	}
-
-	// defer utils.ParseConfigs()()
-	// fmt.Println("Starting")
-	// handler, err := pcap.OpenOffline("./capture.pcap")
-	//
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	// defer handler.Close()
-	//
-	//	for {
-	//		data, _, err := handler.ReadPacketData()
-	//		if err != nil {
-	//			log.Fatal(err)
-	//		}
-	//		packet := gopacket.NewPacket(data, layers.LinkTypeEthernet, gopacket.Default)
-	//		ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-	//		if ethernetLayer == nil {
-	//			log.Println("No Ethernet Layer Found")
-	//			continue
-	//		}
-	//
-	//		ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	//		if ipLayer == nil {
-	//			log.Println("No IP Layer Found")
-	//			continue
-	//		}
-	//
-	//		tcpLayer := packet.Layer(layers.LayerTypeTCP)
-	//		if tcpLayer == nil {
-	//			log.Println("No TCP Layer Found")
-	//			continue
-	//		}
-	//
-	//		ip, _ := ipLayer.(*layers.IPv4)
-	//		if ip.SrcIP.String() == "10.139.0.6" {
-	//			continue
-	//		}
-	//		tcp, _ := tcpLayer.(*layers.TCP)
-	//
-	//		switch tcp.SrcPort {
-	//		case 22:
-	//			continue
-	//		case 443:
-	//			continue
-	//		case 25061:
-	//			continue
-	//		}
-	//		switch tcp.DstPort {
-	//		case 22:
-	//			continue
-	//		case 443:
-	//			continue
-	//		case 25061:
-	//			continue
-	//		}
-	//		// if tcp.SrcPort tcp.DstPort != 22 {
-	//		log.Printf("%s:%d -> %s:%d ", ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort)
-	//		// TODO: Filter for HTTP Traffic
-	//		// TODO: Alanyze HTTP Headers
-	//		// TODO: Display HTTP Body
-	//
-	// }
 }
